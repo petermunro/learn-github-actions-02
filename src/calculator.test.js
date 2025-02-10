1a3564737ca524a2fac64dda6e4682b6d3a28310
Peter Munro <peter@petermunro.org>
2025-02-10 22:24:28 +0000

    Initial setup with Node.js and Jest tests

---
+++ src/calculator.test.js
@@ -0,0 +1,10 @@
+const { add } = require('./calculator');
+
+test('adds 1 + 2 to equal 3', () => {
+    expect(add(1, 2)).toBe(3);
+});
+
+test('adds 5 + 7 to equal 12', () => {
+    expect(add(5, 7)).toBe(12);
+});
+
